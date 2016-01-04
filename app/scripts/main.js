42812adc3d8b06b6153788fe7c25e7f0c636b0d3
Fumio SAGAWA <fumio.sagawa@gmail.com>
2016-01-04 17:01:15 +0900

    updateready event listener

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('ashiras.xyz.xyz');
+console.log('ashiras.xyz');
+
+(function (){
+  // Check if a new cache is available on page load.
+  window.addEventListener('load', function(e) {
+    window.applicationCache.addEventListener('updateready', function(e) {
+      if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+        // Browser downloaded a new app cache.
+        // Swap it in and reload the page to get the new hotness.
+        window.applicationCache.swapCache();
+        if (confirm('A new version of this site is available. Load it?')) {
+          window.location.reload();
+        }
+      } else {
+        // Manifest didn't changed. Nothing new to server.
+        console.log('Manifest didn\'t changed. Nothing new to server.');
+      }
+    }, false);
+  }, false);
+})();
